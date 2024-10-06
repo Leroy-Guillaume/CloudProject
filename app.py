0b1408d7c2f82eef50fdb5c0f42c60f90a884f97
Leroy-Guillaume <70961560+Leroy-Guillaume@users.noreply.github.com>
2024-10-06 19:08:28 +0200

    up

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World !"
+@app.route('/')
+def hello_world():
+    return 'Welcome to my Python API, by Aurélien Rogé'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
